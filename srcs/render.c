c0f24c27236676ac7a7feedce703ccaee69eb789
Hicham Es-Saquy <hes-saqu@c3r8p8.1337.ma>
2024-12-11 13:57:33 +0100

    stopped at vertical intersecion

--- srcs/render.c
+++ srcs/render.c
@@ -1,4 +1,9 @@
 #include "../includes/cub3d.h"
+#include <math.h>
+#define M_PI   3.14159265358979323846264338327950288
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
+
 // each wall is 64*64.
 int map[8][8]={
 {1, 1 , 1, 1, 1, 1, 1, 1},
@@ -20,22 +25,26 @@ double   horizontalraycast(t_data *data, int stripex, double castAngle)
     //ya iknowns ca isnt
     double len;
     int hit;
-    double cx;
+    double cx;// should be double from small distances
     double cy;
     hit = 0;
+    castAngle = 170;
     // raydiry
-    castAngle = 230; // for testing
+    // FIX ROUNDING ERROR OF FLOAING POOINTS.
     if (castAngle >= 0 && castAngle <= 180)// going up handle if dir y = 0
     {
-        printf("upward ray %d\n");
+        printf("upward ray\n");
         // defining the first horizontal intersection
         cy = ((int)data->player->posy / texheight) * texheight - 1;
-        cx = (data->player->posy - cy) / tan(castAngle);
+        //printf("primary cy is %d\n", cy);
+        //printf("tan value tan(75) %f\n", tan(degToRad(castAngle)));
+        cx = roundf(data->player->posy - cy) / tan(degToRad(castAngle));// this can be so small --> 0 ?
+        //printf("tan cx is %d\n", cx);
         cx += data->player->posx;
-        /*printf("cy is %f cx is %f\n", cy, cx);
-        printf("first cube horizontal intersection is at x:%d y:%d \n", ((int)cx/64)
-        , ((int)cy / 64));
-        exit(1);*/
+        //printf("cy is %f cx is %f\n", cy, cx);
+        //printf("first cube horizontal intersection is at x:%d y:%d \n", (int)(cx/64)
+        //, (int)(cy / 64));
+        //exit(1);
     }
     
     // ray going down // shouldn't let angle be nagtive : 0-->360;
@@ -44,27 +53,84 @@ double   horizontalraycast(t_data *data, int stripex, double castAngle)
         // handle angle = 0 ; 270 180 ; 90
         printf("down ward ray\n");
         cy = ((int)data->player->posy / texheight) * texheight + texheight;
-        cx = (cy - data->player->posy) / tan(castAngle);
-        printf("cy is %f cx is %f\n", cy, cx);
+        cx = roundf(cy - data->player->posy) / tan(degToRad(castAngle));
+        //printf("cy is %f cx is %f\n", cy, cx);
         if (castAngle > 270)
             cx += data->player->posx;
         else if (castAngle < 270)
             cx = data->player->posx - cx;
-        printf("first cube horizontal intersection is at x:%d y:%d \n", ((int)cx/64)
-        , ((int)cy / 64));
-        exit(1);
+        //cx = roundf(cx) / 64;
+        //cy = roundf(cy) / 64;
+        //printf("first cube horizontal intersection is at x:%d y:%d \n", ((int)cx/64)
+        //, ((int)cy / 64));
+        //exit(1);
     }
+    printf("first cube horizontal intersection is at x:%d y:%d \n", (int)cx / 64,
+        (int)cy / 64);
+    if (map[(int)cx / 64][(int)cy / 64] != 0)
+        hit = 1;
     while (hit != 1)
     {
         printf("entered  hit loop\n");
-        exit(1);
+        // traverse upward
+        if (castAngle >= 0 && castAngle <= 180)
+        {
+            cy -= texheight;
+            cx += texheight / tan(degToRad(castAngle));
+        }
+        else
+        {
+            cy +=texheight;
+            if (castAngle > 270)
+                cx += texheight / tan(degToRad(castAngle));
+            else if (castAngle < 270)
+                cx -= texheight / tan(degToRad(castAngle));
+        }
+        if (map[(int)cx / 64][(int)cy / 64] != 0)
+            hit = 1;
     }
-    return (0.0);
+    
+    printf("found a wall at %d %d\n", (int)cx / 64, (int)cy / 64);
+    double distance = 0.0; // to be calculated out of cx and yx
+    //exit(1);
+    return (distance);
 }
 
 double   verticalraycast(t_data *data, double stripex, double castAngle)
 {
-    double len;
+    double  len;
+    double  cx;
+    double  cy;
+    int     hit;
+    // defining first intersection y axis
+    // if raydirx > 0 going right side;
+    if ((castAngle > 0 && castAngle < 90)
+        || (castAngle > 270 && castAngle < 360))
+    {
+        // finding  adjacent  : current + 1
+        cx = ((int)data->player->posx / texwidth) * texwidth + texwidth; // box x
+        // small distance
+        cy = fabs(tan(degToRad(castAngle)) * roundf(cx - data->player->posx));// opposent : round down second part?
+        if (castAngle > 0 && castAngle < 90)
+            cy += data->player->posy; // box y;
+        else
+            cy -= data->player->posy;
+    }
+    else
+    {
+        // adjacent current - box
+        cx = ((int)data->player->posx - texwidth) * texwidth - 1; // box smaller
+        // small disance :opposent : tan will be negative ?
+        cy =  fabs(tan(degToRad(castAngle) * roundf(data->player->posx - cx)));
+        // fabs (tan) ? because we decided when to add and -
+        /// !!!! when to add and when to substract to cy
+        //if (castAngle > 0 && castAngle < 90) // handled in the first if
+        if (castAngle > 90 && castAngle < 180)
+            cy += data->player->posy;
+        else
+            cy -= data->player->posy;
+    }
+    
     return (0.0);
 }
 

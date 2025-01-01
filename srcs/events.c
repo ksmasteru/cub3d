a0df313b5fa5da52f13de22b2480a119c51d1099
Hicham Es-Saquy <hes-saqu@c3r9p5.1337.ma>
2025-01-01 13:55:22 +0100

    player_move_up fix

--- srcs/events.c
+++ srcs/events.c
@@ -21,21 +21,25 @@ bool player_move_up(t_data *data, double old_posx, double old_posy, double ratio
 	int box_y;
 	int	wally;
 	int wallx;
+	double	original_posx;
+	double	original_posy;
 
+	original_posx = old_posx;
+	original_posy = old_posy;
 	t_ray ray;
     update_ray_dir(&(data->ray), data->player->view_deg);
 	ray = data->ray;
 	if (ray.dir_x > 0) /*here*/
 		data->player->posx += fabs(MOVE_SPEED * ratio * cos(degToRad(data->player->view_deg))); // MOVE SPEED VALUE ?
 	else if (ray.dir_x < 0)
-		data->player->posx -=  fabs(MOVE_SPEED * ratio * cos(degToRad(data->player->view_deg)));
+		data->player->posx -= fabs(MOVE_SPEED * ratio * cos(degToRad(data->player->view_deg)));
 	if (ray.dir_y > 0)
 		data->player->posy += fabs(MOVE_SPEED * ratio * sin(degToRad(data->player->view_deg))); // sin at this degree is negative
 	else if (ray.dir_y < 0)
 		data->player->posy -= fabs(MOVE_SPEED * ratio * sin(degToRad(data->player->view_deg)));
-	// check if its inside a wall. if yes reverse
-	//printf("new player posx %d posy %d\n", (int)data->player->posx, (int)data->player->posy);
-	// first check if its out of bounds. .. ?????
+	/*check if its inside a wall. if yes reverse
+	printf("new player posx %d posy %d\n", (int)data->player->posx, (int)data->player->posy);
+	first check if its out of bounds. .. ?????*/
 	if (data->player->posx > map_h * texwidth)
 		data->player->posx = map_h  * texwidth - 1;
 	else if (data->player->posx < 0)
@@ -44,83 +48,37 @@ bool player_move_up(t_data *data, double old_posx, double old_posy, double ratio
 		data->player->posy = map_h * texheight - 1;
 	else if (data->player->posy < 0)
 		data->player->posx = 1;
-	/*wall slidig test here*/
-	if (ray.dir_x == 0)
+	old_posx = data->player->posx;
+	old_posy = data->player->posy;
+	int i = 0;
+	while (i < 20 && map[(int)data->player->posy / 64][(int)data->player->posx / 64] == 0)
 	{
-		// get the corrdinates of the next wall.
-		wallx = (int)data->player->posx / 64 * 64;
-		if (ray.dir_y > 0 )// going down
-			wally = (int)data->player->posy / 64 * 64 + 64;
-		else
-			wally = (int)data->player->posy / 64 * 64 - 1;
-		// !! IF NEXT GRID IS A WALL. AND the distance is less than 20 make it 20
-		printf("wall y is %d wall x is %d\n", wally , wallx);
-		if (map[wally / 64][(int)data->player->posx / 64] != 0 && abs (wally - (int)data->player->posy) < 20)
-		{
-			printf("condition met\n");
-			if (ray.dir_y > 0)
-				data->player->posy = wally - 20;
-			else
-				data->player->posy = wally + 20;
-			return (true);
-		}
+		if (ray.dir_x > 0) /*here*/
+			data->player->posx += 1 * fabs(cos(degToRad(data->player->view_deg))); // MOVE SPEED VALUE ?
+		else if (ray.dir_x < 0)
+			data->player->posx -= 1 * fabs(cos(degToRad(data->player->view_deg)));
+		if (ray.dir_y > 0)
+			data->player->posy += 1 * fabs(sin(degToRad(data->player->view_deg))); // sin at this degree is negative
+		else if (ray.dir_y < 0)
+			data->player->posy -= 1 * fabs(sin(degToRad(data->player->view_deg)));
+		i++;
 	}
-	if (ray.dir_y == 0)
+	if (i == 20)
 	{
-		// coordinate of next wall
-		wally = (int)data->player->posy / 64 * 64;
-		if (ray.dir_x > 0) // going rihgt;
-			wallx = (int)data->player->posx / 64 * 64 + 64;
-		else
-			wallx = (int)data->player->posx / 64 * 64 - 1;
-		if (map[wally / 64][wallx / 64] != 0 && abs(wallx - (int)data->player->posx) < 20)
-		{
-			printf("condition met\n");
-			if (ray.dir_x > 0)
-				data->player->posx = wallx - 20;
-			else
-				data->player->posx = wallx + 20;
-		}
-		return (true);
- 	}
-	
-	// if is up 
-	// try it at  raydirx = 0 and ray dir y = 0
-	/*if (map[(int)data->player->posy / texheight][(int)data->player->posx / texwidth] != 0
-	 			&& (ray.dir_x == 0))
-	{
-		// this get trigered when the player gets trigered when the player gets inside the wall.
-		// you should check if the distance is less than 12 then make it 12 dit should be tested before
-			printf("condition met\n");
-			data->player->posx = old_posx;
-			if (ray.dir_y > 0) // going down.
-			{
-				wally = (int)data->player->posy / texheight * texheight;
-				if (abs(wally - (int)data->player->posy) < 30)
-					data->player->posy = wally - 30;
-			}
-			else
-			{
-				wally = (int)data->player->posy / texheight * texheight + texheight;
-				if (abs(wally - (int)data->player->posy) < 30)
-					data->player->posy= wally + 30;
-			}
-			return (true);
-	}*/
-
-	if (map[(int)data->player->posy / texheight][(int)data->player->posx / texwidth] != 0)/*wall*/ // it could be equal to -1 : outofpbound
-	{
-		// move the player back. at 28 units from the wall wchi side ? x, y both ?
-		// units to substract from x position
-		data->player->posx = old_posx;// or increment by the necessary value. % distance.
+		printf("player isnt close to a wall by 20 units\n");
+		data->player->posx = old_posx;
 		data->player->posy = old_posy;
-		return (false); // dont update img.
+		return (true);
 	}
-	//map[(int)data->player->posy / texwidth][(int)data->player->posx / texwidth] = 6;
-	// anD OLD POSITION SHOULD BE 0
-	//map[(int)old_posy / texwidth][(int)old_posx / texwidth] = 0;
-	return (true);
+	else
+	{
+		data->player->posx = original_posx;
+		data->player->posy = original_posy;
+		// here slide IN!
+	}
+	return (false);
 }
+
 
 // MOVE DOWN IF NO WALL IS BEHIND THE PLAYER. : inverse of move_up semantics
 // of increment/decrementing pos

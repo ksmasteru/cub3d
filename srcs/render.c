734d7fbe7780c8f38d74da26b1544ac0671f1b1e
Hicham Es-Saquy <hes-saqu@e2r10p3.1337.ma>
2025-02-21 18:42:09 +0100

    render.c lines

--- srcs/render.c
+++ srcs/render.c
@@ -90,7 +90,6 @@ void    put_wall_side(t_data *data, int stripex, double distance, int side)
 
     pixdata.slice_height = ((double)texheight / distance) * (projection_d); // this should be round up distance too small ? 
     pixdata.x_offset = (int)data->player->ver_hitx[stripex] % (texwidth - 1); // logical error. : should get actual position of the hit.
-    //printf("for stripe %d of type 0 x offset is %d hit %f\n", stripex, x_offset, data->player->hitx);
     pixdata.step = 1.0 * texwidth / pixdata.slice_height;
     pixdata.y_min =  SCREEN_H / 2 - pixdata.slice_height / 2;
     if (pixdata.y_min < 0)
@@ -186,98 +185,37 @@ void    put_walls(t_data *data)
     }
 }
 
-/*double   raycast(t_data *data, double castAngle, int *side)
-{
-    double horizontalray;
-    double verticalray;
-    double  ver_hitx;
-    double  ver_hity;
-    if (castAngle < 0)
-        castAngle += 360.0;
-    if (castAngle == 0 || castAngle == 180.0 || castAngle == 360)
-    {
-        *side = 1;
-        return (x_axis_raycast(data, castAngle));
-    }
-    else if (castAngle == 90.0 || castAngle == 270.0)
-    {
-        *side = 0;
-        return (y_axis_raycast(data, castAngle));
-    }
-    verticalray = verticalraycast(data, castAngle);
-    ver_hitx = data->player->hitx;
-    ver_hity = data->player->hity;
-    horizontalray = horizontalraycast(data, castAngle);
-    if (verticalray < horizontalray)
-    {
-        printf("for angle %f vertical height is %f because horizontal is %f\n", castAngle, verticalray, horizontalray);
-        *side = 0;
-        data->player->hitx = ver_hitx;
-        data->player->hity = ver_hity;
-        return (verticalray) * cos(degToRad(data->player->beta_angle));
-    }
-    *side = 1;
-    //}
-    //printf("distance to the wall is %f\n", horizontalray);
-    //printf("corrected distance to the wall is %f\n", horizontalray * cos(degToRad(data->player->beta_angle)));
-    printf("for angle %f horizontal height is %f because vertical was %f\n", castAngle, horizontalray,verticalray);
-    return (horizontalray) * cos(degToRad(data->player->beta_angle));
-}*/
-
 void set_new_img(t_data *data)
 {
-    //if (data->img->mlx_img != NULL)/*here dont destroy the img untill the new img is ready*/
-    //{
-      //  mlx_destroy_image(data->mlx_ptr, data->img->mlx_img);
-       // mlx_clear_window(data->mlx_ptr, data->win_ptr);
-        //free(data->img->mlx_img);
-        //data->img->mlx_img = NULL;
-    //}
     data->img->mlx_img = mlx_new_image(data->mlx_ptr, SCREEN_W, SCREEN_H);
     data->img->adrs = mlx_get_data_addr(data->img->mlx_img, &(data->img->bpp), &(data->img->size_line), &(data->img->endian));
 }
 
 int render_walls(t_data *data)
 {
-    //double wallheight;
     int i;
     double  sep_angle;
     double   castAngle;
-    int side;
     
     i = 0;
-    //fflush(stdout);
-    //printf("-------------------player view is %f---------------------\n", data->player->view_deg);
     sep_angle = (double)FOW / SCREEN_W;
     angle = data->player->view_deg;
     castAngle = data->player->view_deg + (double)FOW / 2;
-    //update_ray_dir(&(data->ray), data->player->view_deg);
-    //printf("cast angle is %f\n", castAngle);
     data->player->beta_angle = (double)FOW / 2; // works better than FOW / 2 somehow.
-    //printf("start boxX[%d] boxY[%d]\n", (int)data->player->posx / 64, (int)data->player->posy / 64);
-    //printf("beta angle is %f\n", data->player->beta_angle);
     set_new_img(data);
     while (i < SCREEN_W)
     {
-        //printf("cast angle is %f\n", castAngle);
         data->player->distance[i] = raycast_1(data, castAngle, data->player->side + i, i);
-        //printf("for cast angle %f wallheight is %f side is %d\n", castAngle, wallheight, side);
-        //printf("stripex is %d side is %d\n", i, side);
-        //if (castAngle > -12 && castAngle < -8)
         castAngle -= sep_angle;
         data->player->beta_angle -= sep_angle;
         i++;
     }
     put_walls(data);
-    // swap images.
-    //mlx_clear_window(data->mlx_ptr, data->win_ptr);
-    // destroy old img.
     if (data->old_img->mlx_img)/*should just be an address n need to allocat*/
         mlx_destroy_image(data->mlx_ptr, data->old_img->mlx_img);
     mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->mlx_img, 0, 0);
     show_player_data(data); // write on top of new img
     put_mini_map(data);
-    // this img becomes the old img
     data->old_img->mlx_img = data->img->mlx_img;
     return (0);
 }
